aec0f72c7623cf114f7cbbed505f7c25c3c573d1
Prateek Bahukhandi <bahukhandiprateek@gmail.com>
2021-07-09 16:35:52 +0530

    Added code for codeforces problem

---
+++ Problems/NumberTheory/1242A-TilePainting.cpp
@@ -0,0 +1,70 @@
+#include<iostream>
+#include<vector>
+#include<cstring>
+#include<limits.h>
+#include<algorithm>
+#include<iomanip>
+#include<map>
+#include<cstring>
+#include<math.h>
+#include<queue>
+#define pb push_back
+#define fo(i,n) for(int i=0;i<n;i++)
+using namespace std;
+#define ll long long int
+#define ld long double
+const ld r = 1e+9;
+#define mod 1000000007
+long long inf = 100000000001;
+#define fast std::ios::sync_with_stdio(false);cin.tie(NULL);
+ll lcm(ll a, ll b) // Function to calculate Lcm
+{
+    return (a / __gcd(a, b)) * b;
+}
+int highestPowerof2(int n) // Highest pow of two
+{
+    int p = (int)log2(n);
+    return (int)pow(2, p);
+}
+
+
+ll solve(ll l, ll r)
+{
+    ll ans = 0;
+    while (l || r)
+    {
+        ans += (r - l);
+        r /= 10;
+        l /= 10;
+    }
+    return ans;
+}
+
+
+int main()
+{
+    ll n;
+    cin >> n;
+
+    ll answer = n;
+    for (ll i = 2; i * i <= n; i++)
+    {
+        if (n % i == 0)
+        {
+            answer = __gcd(answer, __gcd(i, n / i));
+        }
+    }
+
+    cout << answer << endl;
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
