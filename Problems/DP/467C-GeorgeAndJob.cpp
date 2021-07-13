5a04af22914f52a65582f8f61cff21d7de748835
Prateek Bahukhandi <bahukhandiprateek@gmail.com>
2021-07-13 15:18:05 +0530

    Added code for codeforces problem

---
+++ Problems/DP/467C-GeorgeAndJob.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long int
+#define fo(i,n) for(int i=0;i<n;i++)
+#define pb push_back
+const int N = 5e4 + 5;
+int dp[5001][5001];
+
+int32_t main()
+{
+    int n, m, k;
+    cin >> n >> m >> k;
+
+    int a[n];
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> a[i];
+    }
+    int sum[n + 1];
+    sum[0] = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        sum[i] = sum[i - 1] + a[i];
+    }
+
+    memset(dp, 0, sizeof(dp));
+    dp[m][1] = sum[m];
+
+    int lim;
+
+    for (int i = m + 1; i <= n; i++)
+    {
+        lim = i / m;
+        for (int j = 1; j <= min(lim, k); j++)
+        {
+
+
+            dp[i][j] = max(dp[i - 1][j], dp[i - m][j - 1] + sum[i] - sum[i - m]);
+
+        }
+    }
+
+
+    cout << dp[n][k] << endl;
+
+
+
+}
+
+
